963b3ed8763baa571b528ee34aa598b8efc7a185
hatem abushaala <hatem.a.abushaala@gmail.com>
2023-09-25 17:40:56 +0300

    add random functionality to middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,50 @@
-export async function onRequest(context) {
-    try {
-        console.log('middleware run')
-      return await context.next();
-    } catch (err) {
-      return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+export async function onRequest({request,env}) {
+    const BASE_URL = env.API_HOST
+    const PAGE_REQUEST_URL =`${BASE_URL}/api/pages`
+
+    const url = new URL(request.url);
+    const pathname = url.pathname.slice(1) // note that path starts with: /
+
+    const sub_pages = await fetchSubPages(PAGE_REQUEST_URL,pathname)
+    // check if page not found
+    if(!sub_pages || sub_pages.error) return new Response('Not found',{status:404})
+
+    // Determine which group this requester is in.
+    const cookie = parse(request.headers.get("Cookie") || "");
+    const sub_page_id_cookie = cookie[pathname] // id of sub page || null
+    if(sub_page_id_cookie){
+      const user_previous_visited_page = sub_pages.find(
+            (sub_page) => sub_page.id == sub_page_id_cookie
+      )
+      // check if page is still exist in the database
+      if(user_previous_visited_page) 
+      return new Response(user_previous_visited_page.html,{
+        headers:{
+          'content-type':'text/html'
+        }
+      }) 
+      
+    //   return Response.redirect(getCFPageUrl(pathname,user_previous_visited_page.id), 301);
+
     }
+
+    const random_page_index = Math.floor(Math.random() * sub_pages.length);
+    const random_page = sub_pages[random_page_index]
+    return new Response(random_page.html,{
+      headers:{
+        'content-type':'text/html',
+        "Set-Cookie":`${pathname}=${random_page.id}; path=/`
+      }
+    }) 
+    // return Response.redirect(getCFPageUrl(pathname,random_page.id), 301);
+
+  }
+
+
+  async function fetchSubPages(requestUrl,pathname){
+    const sub_pages = await fetch(`${requestUrl}/${pathname}`)
+    return sub_pages.json()
+  }
+  function getCFPageUrl(main_page_slug,sub_page_id){
+    return `https://direct-upload-demo-10q.pages.dev/${main_page_slug}/${sub_page_id}`
   }
