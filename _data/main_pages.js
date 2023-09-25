8d74e794fa14a0ee3746f479ef956deb1f1bf308
hatem abushaala <hatem.a.abushaala@gmail.com>
2023-09-25 16:53:12 +0300

    update strapi url

--- _data/main_pages.js
+++ _data/main_pages.js
@@ -8,7 +8,7 @@ module.exports = async function () {
   //   { "name": "Venusaur" }
   // ]
     // TODO change it to strapi server
-  const result = await fetch('https://7f32-78-163-170-147.ngrok-free.app/api/all-pages');
+  const result = await fetch('https://590b-78-183-106-175.ngrok.io/api/all-pages');
   const allPages = await result.json()
   return allPages.flatMap(p=>{
     return p.pages.map(sp=>({...sp,path:`${p.slug}/${sp.id}`}))
